34fafada9f440235b75791ab3d6eeb664f4f69f2
Nico Rithner <nicorithner@gmail.com>
2021-02-23 14:21:55 -0700

    Add S3 to initializer

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,18 +1,28 @@
-require "shrine"
-require "shrine/storage/file_system"
-require "shrine/storage/memory"
-if  Rails.env.test?
+require 'shrine'
+require 'shrine/storage/file_system'
+require 'shrine/storage/memory'
+require 'shrine/storage/s3'
+
+if Rails.env.test?
   Shrine.storages = {
     cache: Shrine::Storage::Memory.new,
-    store: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new
+  }
+elsif Rails.env.development?
+  Shrine.storages = { cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
+                      store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
   }
 else
- Shrine.storages = {
-   cache: Shrine::Storage::FileSystem.new("public", prefix:     "uploads/cache"), # temporary
-   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
- }
+  s3 = Shrine::Storage::S3.new(
+    bucket: 'iklf-user-avatars', # required
+    region: 'us-east-1', # required
+    access_key_id: ENV['S3_ACCESS_KEY'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+  )
 end
-Shrine.plugin :activerecord    # loads Active Record integration
+
+#------ Plugins
+Shrine.plugin :activerecord # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data  # extracts metadata for assigned cached files
 Shrine.plugin :validation_helpers
