5872e0b32669d60719cc007a0ed1aca97d50d746
Nico Rithner <nicorithner@gmail.com>
2021-02-23 10:10:29 -0700

    Add title column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_174250) do
+ActiveRecord::Schema.define(version: 2021_02_22_185042) do
 
   create_table "articles", force: :cascade do |t|
     t.text "body"
     t.text "image_data"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
