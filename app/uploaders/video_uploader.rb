ef0e1499b632a63c405bd05747706b7014487675
Nico Rithner <nicorithner@gmail.com>
2021-02-23 14:19:58 -0700

    Add video uploader (commented out for now)

---
+++ app/uploaders/video_uploader.rb
@@ -0,0 +1,14 @@
+#require "streamio-ffmpeg"
+# 
+#class VideoUploader < Shrine
+#  Attacher.derivatives do |original|
+#    transcoded = Tempfile.new ["transcoded", ".mp4"]
+#    screenshot = Tempfile.new ["screenshot", ".jpg"]
+# 
+#    movie = FFMPEG::Movie.new(original.path)
+#    movie.transcode(transcoded.path)
+#    movie.screenshot(screenshot.path)
+# 
+#    { transcoded: transcoded, screenshot: screenshot }
+#  end
+#end
