d776e5bc31615bbe423de726b6c20827549d46b0
Nico Rithner <nicorithner@gmail.com>
2021-03-09 19:43:42 -0700

    Change to PG, dropped db and ran migrations

--- db/migrate/20210222174250_create_articles.rb
+++ db/migrate/20210222174250_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.text :body
       t.text :image_data
-
+      t.string :title
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_185042) do
+ActiveRecord::Schema.define(version: 2021_02_22_174250) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.text "body"
     t.text "image_data"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "title"
   end
 
 end
